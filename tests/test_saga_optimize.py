c7b056103f6c1f0feff86c95ceb2a2c279a940ad
Huan Jin <hji236@g.uky.edu>
2019-03-05 11:14:18 -0500

    Updated test file

--- tests/test_saga_optimize.py
+++ tests/test_saga_optimize.py
@@ -1,19 +1,36 @@
 import SAGA_optimize
+import math
+
+
+def energyCalculation(elements):
+    energy = 0
+    for index in range(0, len(elements)):
+        energy += abs(index + 1 - elements[index])
+    return energy
 
 
 def test_saga_optimize():
 
-    # saga = SAGA_optimize.SAGA(stepNumber=100000, temperatureStepSize=100, startTemperature=0.5, 
-    #                           alpha=1, direction=-1, energyCalculateion=energyCalculation, crossoverRate=0.5, 
-    #                           mutationRate=3, annealMutationRate=1, populationSize=20)
+    saga = SAGA_optimize.SAGA(stepNumber=100000, temperatureStepSize=100, startTemperature=0.5,
+                              alpha=1, direction=-1, energyCalculateion=energyCalculation, crossoverRate=0.5,
+                              mutationRate=3, annealMutationRate=1, populationSize=20)
 
-    # saga.addElementDescriptions(SAGA_optimize.ElementDescription(low=0, high=10), 
-    #                             SAGA_optimize.ElementDescription(low=0, high=10), 
-    #                             SAGA_optimize.ElementDescription(low=0, high=10), 
-    #                             SAGA_optimize.ElementDescription(low=0, high=10), 
-    #                             SAGA_optimize.ElementDescription(low=0, high=10))
+    saga.addElementDescriptions(SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10),
+                                SAGA_optimize.ElementDescription(low=0, high=10))
+
     
-    # optimized_population = saga.optimize()
+    optimized_population = saga.optimize()
+    bestGuess = optimized_population.bestGuess
 
-    # assert optimized_population == expected_population
-    assert True
+    for i in range(len(bestGuess.elements)):
+        assert abs(bestGuess.elements[i] - 1 - i) < math.pow(10, -6)
+
+
+
