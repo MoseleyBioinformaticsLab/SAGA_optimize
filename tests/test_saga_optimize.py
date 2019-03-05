b9b0eb32352497d98aea70355c723497f254a8db
Huan Jin <hji236@g.uky.edu>
2019-03-05 11:18:18 -0500

    Updated test file

--- tests/test_saga_optimize.py
+++ tests/test_saga_optimize.py
@@ -12,7 +12,7 @@ def energyCalculation(elements):
 def test_saga_optimize():
 
     saga = SAGA_optimize.SAGA(stepNumber=100000, temperatureStepSize=100, startTemperature=0.5,
-                              alpha=1, direction=-1, energyCalculateion=energyCalculation, crossoverRate=0.5,
+                              alpha=1, direction=-1, energyCalculation=energyCalculation, crossoverRate=0.5,
                               mutationRate=3, annealMutationRate=1, populationSize=20)
 
     saga.addElementDescriptions(SAGA_optimize.ElementDescription(low=0, high=10),
